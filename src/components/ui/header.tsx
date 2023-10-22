123bc5d5e1bc83a2dfcc26eab5e3361563071851
Andre Juan <andrjetx@yahoo.com.br>
2023-10-22 19:07:23 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,54 @@
 import { Card } from "./card";
 import { Button } from "./button";
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { MenuIcon,
+        ShoppingCartIcon,
+        LogInIcon,
+        PercentIcon,
+        HomeIcon,
+        ListOrderedIcon
+        } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 
 const Header = () => {
     return (
         <Card className="flex justify-between p-[1.875rem] items-center">
-            <Button size="icon" variant="outline">
-                <MenuIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+
+                <SheetContent side={"left"}>
+                    <SheetHeader className="text-left text-lg font-semibold">
+                        MENU
+                    </SheetHeader>
+
+                    <div className="mt-2 flex flex-col gap-2">
+                        <Button variant={"outline"} className="w-full justify-start gap-2">
+                            <LogInIcon size={16} />
+                            Fazer Login 
+                        </Button>
+
+                        <Button variant={"outline"} className="w-full justify-start gap-2">
+                            <HomeIcon size={16} />
+                            Inicio 
+                        </Button>
+
+                        <Button variant={"outline"} className="w-full justify-start gap-2">
+                            <PercentIcon size={16} />
+                            Ofertas 
+                        </Button>
+
+                        <Button variant={"outline"} className="w-full justify-start gap-2">
+                            <ListOrderedIcon size={16} />
+                            Catalogo
+                        </Button>
+                    </div>
+
+                </SheetContent>
+            </Sheet>
 
             <h1 className="font-semibold text-lg"><span className="text-primary">AJ</span> Store</h1>
 
