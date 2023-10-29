5efeea37ecd00e30f3ec5b16dcdef9fe15b40104
Andre Juan <andrjetx@yahoo.com.br>
2023-10-29 09:24:02 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="bg-accent px-8 py-4 text-[10px] opacity-75">
+            © 2023 Copyright <span className="font-semibold">AJ Store</span>
+        </div>
+    );
+}
+
+export default Footer;
